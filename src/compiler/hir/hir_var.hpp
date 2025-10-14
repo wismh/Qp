61736ea1c754a6a93406ad98a528798143c38f98
wismh <68060501+wismh@users.noreply.github.com>
2025-10-14 14:00:00 +0300

    qp-70 feat: typecheck named fn values and overloads
    Unique fns coerce to fn types; overloads need an expected type.

--- src/compiler/hir/hir_var.hpp
+++ src/compiler/hir/hir_var.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include "compiler/type.hpp"
 #include <string>
+#include <vector>
 
 namespace qpc {
 
 struct HirVar {
     std::string name;
+    std::vector<Type> type_args;
+    bool fn_value = false;
 };
 
 }  // namespace qpc
